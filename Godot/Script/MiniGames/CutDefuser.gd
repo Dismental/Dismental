b06209d703ab11ff1b57b23f34850eecdd56a622
Pepijn <p.e.f.klop@student.tudelft.nl>
2020-05-14 12:43:05 +0200

    Add restart popup

--- Godot/Script/MiniGames/CutDefuser.gd
+++ Godot/Script/MiniGames/CutDefuser.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 onready var start_dialog = $Control/StartDialog
+onready var game_over_dialog = $Control/GameOverDialog
 onready var completed_dialog = $Control/CompletedDialog
 
 const Utils = preload("res://Script/Utils.gd")
 
 var x_value_completed = 1900
+var start_x_ratio = 0.07
 var map_sprite
 var dots = []
 var running = false
@@ -13,8 +15,11 @@ var start_position
 
 func _ready():
 	start_dialog.popup()
-	start_position =  Vector2(get_viewport().size.x * 0.07 , get_viewport().size.y / 2)
-	_load_map(2, false)
+	
+	var vp_size = get_viewport().size
+	start_position =  Vector2(vp_size.x * start_x_ratio, vp_size.y / 2)
+	
+	_load_map(2)
 
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,26 +32,26 @@ func _process(_delta):
 
 func _draw():
 	var mouse_pos = _get_mouse_pos()
-	if running:
-		# Add mousepots to list of coordinates
-		if len (dots) == 0 or !dots[len(dots)-1] == mouse_pos:
-			dots.append(mouse_pos)
+
+	# Add mousepos to list of past mouse position
+	# if the previous mouse position wasn't the same
+	if len (dots) == 0 or !dots[len(dots)-1] == mouse_pos:
+		dots.append(mouse_pos)
 	
 	# Draw line
-	for i in range(1, len(dots) - 2):
+	for i in range(2, len(dots) - 1):
 		draw_line(dots[i], dots[i+1],  Color(1, 0, 0), 10)
 		
 	# Draw Start Point
 	var vp_rect = get_viewport_rect().size
-	var start_point = Vector2(vp_rect.x * 0.07, vp_rect.y/2)
+	var start_point = Vector2(vp_rect.x * start_x_ratio, vp_rect.y/2)
 	draw_circle(start_point, 50, Color(0, 0, 1))
 	
-	if running:
-		# Draw current pointer
-		if len (dots) >= 2:
-			var rad = 25
-			var col = Color(0, 1, 0) if _is_mouse_on_track() else Color(1, 0, 0)
-			draw_circle(mouse_pos, rad, col)
+	# Draw current pointer
+	if len (dots) > 2:
+		var rad = 25
+		var col = Color(0, 1, 0) if _is_mouse_on_track() else Color(1, 0, 0)
+		draw_circle(mouse_pos, rad, col)
 
 
 func _unhandled_input(event):
@@ -55,13 +60,14 @@ func _unhandled_input(event):
 			get_tree().quit()
 
 func _game_over():
-	_move_mouse_to_start()
-	dots.clear()
+	running = false
+	game_over_dialog.popup()
 	
 
 func _update_game_state():
-	if !_is_mouse_on_track() or _get_mouse_pos().x < 0:
-		_game_over()
+	if len(dots) > 2:
+		if !_is_mouse_on_track() or _get_mouse_pos().x < 0:
+			_game_over()
 	elif _get_mouse_pos().x > x_value_completed:
 		_game_completed()
 
@@ -130,5 +136,11 @@ func _on_StartDialog_confirmed():
 	running = true
 	_move_mouse_to_start()
 
+func _restart_game():
+	_move_mouse_to_start()
+	dots.clear()
+	running = true
 
-
+func _on_GameOverDialog_confirmed():
+	_restart_game()
+	
