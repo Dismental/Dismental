b54770d19fc28a7b183f5a0d19075357d0dd4ff6
Jeroen Janssen <j.d.janssen@student.tudelft.nl>
2020-05-16 13:14:49 +0200

    Rename Lobby global to Network global and fix project settings

--- Godot/Script/P2pExample.gd
+++ Godot/Script/P2pExample.gd
@@ -7,19 +7,19 @@ var _input_ip = ''
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Lobby.text_label = get_node("Label")
+	Network.text_label = get_node("Label")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _on_create_server():
-	Lobby._create_server()
+	Network._create_server()
 
 
 func _on_create_client_pressed():
 	print(_input_ip)
-	Lobby._create_client(_input_ip)
+	Network._create_client(_input_ip)
 
 
 func _on_ip_changed(new_text):
