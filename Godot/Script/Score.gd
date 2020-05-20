0ff6421782226d9348089592d436ccaba3fba948
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-05-20 17:40:22 +0200

    Added save function to Score

--- Godot/Script/Score.gd
+++ Godot/Script/Score.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Object
 class_name Score
 
 var team : String 
@@ -6,17 +6,24 @@ var level : String
 var time : String
 var date : String
 
-func _init(teamName: String, levelName: String, completedTime: String, completedDate: String):
-	add_to_group("Persist")
-	team = teamName
-	level = levelName
-	time = completedTime
-	date = completedDate
+func _init(team_name: String, level_name: String, completed_time: String, completed_date: String):
+	team = team_name
+	level = level_name
+	time = completed_time
+	date = completed_date
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _save():
+	var save_dict = {
+		"team" : team,
+		"level" : level,
+		"time" : time,
+		"date" : date,
+	}
+	return save_dict
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
