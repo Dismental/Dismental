da1646765eea58aa32bfcd9946e6552c40865127
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-05-20 17:43:08 +0200

    Changed way MainMenu goes to ScoreBoard

--- Godot/Script/SceneManagers/MainMenuSceneManager.gd
+++ Godot/Script/SceneManagers/MainMenuSceneManager.gd
@@ -10,10 +10,16 @@ func _on_SettingsButton_pressed():
 	change_scene("res://Scenes/SettingsScene.tscn")
 
 func _on_ScoreBoardButton_pressed():
-	change_scene("res://Scenes/ScoreScenes/Scoreboard.tscn")
+	var node = preload("res://Scenes/ScoreScenes/Scoreboard.tscn")
+	node = node.instance()
+	get_parent().add_child(node)
+	get_parent().remove_child(self)
+	queue_free()
+	if is_queued_for_deletion():
+		print("deleting")
+	else:
+		push_error("can't delete" + str(self))
 
 func change_scene(filename):
 	if get_tree().change_scene(filename) != OK:
 		print("Error loading " + str(filename))
-
-
