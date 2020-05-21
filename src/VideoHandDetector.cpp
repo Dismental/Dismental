a1254f05808a44844ca1368b92b544793677d286
Gees Brouwer <G.D.Brouwer@student.tudelft.nl>
2020-05-22 01:33:14 +0200

    Added VideoHandDetector class

---
+++ src/VideoHandDetector.cpp
@@ -0,0 +1,357 @@
+#include "VideoHandDetector.h"
+#include <iostream>
+#include <opencv2/imgproc.hpp>
+#include <GodotGlobal.hpp>
+#include <queue>
+
+const double VideoHandDetector::TICK_FREQUENCY = cv::getTickFrequency();
+
+// VideoHandDetector::VideoHandDetector(const std::string cascadeFilePath, cv::VideoCapture &videoCapture)
+// {
+//     setFaceCascade(cascadeFilePath);
+//     setVideoCapture(videoCapture);
+// }
+
+VideoHandDetector::VideoHandDetector() {
+    
+}
+
+void VideoHandDetector::setVideoCapture(cv::VideoCapture &videoCapture)
+{
+    m_videoCapture = &videoCapture;
+}
+
+cv::VideoCapture *VideoHandDetector::videoCapture() const
+{
+    return m_videoCapture;
+}
+
+void VideoHandDetector::setFaceCascade(const std::string cascadeFilePath)
+{
+    if (m_faceCascade == NULL) {
+        m_faceCascade = new cv::CascadeClassifier(cascadeFilePath);
+    }
+    else {
+        m_faceCascade->load(cascadeFilePath);
+    }
+
+    if (m_faceCascade->empty()) {
+        std::cerr << "Error creating cascade classifier. Make sure the file" << std::endl
+            << cascadeFilePath << " exists." << std::endl;
+    }
+}
+
+cv::CascadeClassifier *VideoHandDetector::faceCascade() const
+{
+    return m_faceCascade;
+}
+
+std::queue<cv::Mat> VideoHandDetector::getLastKnownFaceTemplateQueue() const
+{
+    return m_faceTemplate_lastKnown_queue;
+}
+
+void VideoHandDetector::setResizedWidth(const int width)
+{
+    m_resizedWidth = std::max(width, 1);
+}
+
+int VideoHandDetector::resizedWidth() const
+{
+    return m_resizedWidth;
+}
+
+bool VideoHandDetector::isFaceFound() const
+{
+	return m_foundFace;
+}
+
+cv::Rect VideoHandDetector::face() const
+{
+    cv::Rect faceRect = m_trackedFace;
+    faceRect.x = (int)(faceRect.x / m_scale);
+    faceRect.y = (int)(faceRect.y / m_scale);
+    faceRect.width = (int)(faceRect.width / m_scale);
+    faceRect.height = (int)(faceRect.height / m_scale);
+    return faceRect;
+}
+
+cv::Point VideoHandDetector::facePosition() const
+{
+    cv::Point facePos;
+    facePos.x = (int)(m_facePosition.x / m_scale);
+    facePos.y = (int)(m_facePosition.y / m_scale);
+    return facePos;
+}
+
+void VideoHandDetector::setTemplateMatchingMaxDuration(const double s)
+{
+    m_templateMatchingMaxDuration = s;
+}
+
+double VideoHandDetector::templateMatchingMaxDuration() const
+{
+    return m_templateMatchingMaxDuration;
+}
+
+VideoHandDetector::~VideoHandDetector()
+{
+    if (m_faceCascade != NULL) {
+        delete m_faceCascade;
+    }
+}
+
+cv::Rect VideoHandDetector::doubleRectSize(const cv::Rect &inputRect, const cv::Rect &frameSize) const
+{
+    cv::Rect outputRect;
+    // Double rect size
+    outputRect.width = inputRect.width * 2;
+    outputRect.height = inputRect.height * 2;
+
+    // Center rect around original center
+    outputRect.x = inputRect.x - inputRect.width / 2;
+    outputRect.y = inputRect.y - inputRect.height / 2;
+
+    // Handle edge cases
+    if (outputRect.x < frameSize.x) {
+        outputRect.width += outputRect.x;
+        outputRect.x = frameSize.x;
+    }
+    if (outputRect.y < frameSize.y) {
+        outputRect.height += outputRect.y;
+        outputRect.y = frameSize.y;
+    }
+
+    if (outputRect.x + outputRect.width > frameSize.width) {
+        outputRect.width = frameSize.width - outputRect.x;
+    }
+    if (outputRect.y + outputRect.height > frameSize.height) {
+        outputRect.height = frameSize.height - outputRect.y;
+    }
+
+    return outputRect;
+}
+
+cv::Point VideoHandDetector::centerOfRect(const cv::Rect &rect) const
+{
+    return cv::Point(rect.x + rect.width / 2, rect.y + rect.height / 2);
+}
+
+cv::Rect VideoHandDetector::biggestFace(std::vector<cv::Rect> &faces) const
+{
+    assert(!faces.empty());
+
+    cv::Rect *biggest = &faces[0];
+    for (auto &face : faces) {
+        if (face.area() < biggest->area())
+            biggest = &face;
+    }
+    return *biggest;
+}
+
+/*
+* Face template is small patch in the middle of detected face.
+*/
+cv::Mat VideoHandDetector::getFaceTemplate(const cv::Mat &frame, cv::Rect face)
+{
+    face.x += face.width / 4;
+    face.y += face.height / 4;
+    face.width /= 2;
+    face.height /= 2;
+
+    cv::Mat faceTemplate = frame(face).clone();
+    return faceTemplate;
+}
+
+void VideoHandDetector::detectFaceAllSizes(const cv::Mat &frame)
+{
+    // Minimum face size is 1/5th of screen height
+    // Maximum face size is 2/3rds of screen height
+    m_faceCascade->detectMultiScale(frame, m_allFaces, 1.1, 3, 0,
+        cv::Size(frame.rows / 5, frame.rows / 5),
+        cv::Size(frame.rows * 2 / 3, frame.rows * 2 / 3));
+
+    if (m_allFaces.empty()) return;
+
+    m_foundFace = true;
+
+    // Locate biggest face
+    m_trackedFace = biggestFace(m_allFaces);
+
+    // Copy face template
+    m_faceTemplate = getFaceTemplate(frame, m_trackedFace);
+
+    // Calculate roi
+    m_faceRoi = doubleRectSize(m_trackedFace, cv::Rect(0, 0, frame.cols, frame.rows));
+
+    // Update face position
+    m_facePosition = centerOfRect(m_trackedFace);
+}
+
+void VideoHandDetector::detectFaceAroundRoi(const cv::Mat &frame)
+{
+    // Detect faces sized +/-20% off biggest face in previous search
+    m_faceCascade->detectMultiScale(frame(m_faceRoi), m_allFaces, 1.1, 3, 0,
+        cv::Size(m_trackedFace.width * 8 / 10, m_trackedFace.height * 8 / 10),
+        cv::Size(m_trackedFace.width * 12 / 10, m_trackedFace.width * 12 / 10));
+
+    if (m_allFaces.empty())
+    {
+        // Activate template matching if not already started and start timer
+        if (!m_templateMatchingRunning) m_trackedFace_lastKnown = m_trackedFace;
+        m_templateMatchingRunning = true;
+        if (m_templateMatchingStartTime == 0)
+            m_templateMatchingStartTime = cv::getTickCount();
+        return;
+    }
+
+    // Add face template to the queue
+    m_faceTemplate_lastKnown_queue.push(getFaceTemplate(frame, m_trackedFace));
+
+    // If queue is larger than [maxQueueSize], pop oldest facetemplate
+    if (m_faceTemplate_lastKnown_queue.size() > m_maxBufferFaceTemplate) m_faceTemplate_lastKnown_queue.pop();
+   
+    // Turn off template matching if running and reset timer
+    m_templateMatchingRunning = false;
+    m_templateMatchingCurrentTime = m_templateMatchingStartTime = 0;
+
+    // Get detected face
+    m_trackedFace = biggestFace(m_allFaces);
+
+    // Add roi offset to face
+    m_trackedFace.x += m_faceRoi.x;
+    m_trackedFace.y += m_faceRoi.y;
+
+    // Get face template
+    m_faceTemplate = getFaceTemplate(frame, m_trackedFace);
+
+    // Calculate roi
+    m_faceRoi = doubleRectSize(m_trackedFace, cv::Rect(0, 0, frame.cols, frame.rows));
+
+    // Update face position
+    m_facePosition = centerOfRect(m_trackedFace);
+}
+
+void VideoHandDetector::detectFacesTemplateMatching(const cv::Mat &frame, const cv::Mat &orgFrame)
+{
+    // Calculate duration of template matching
+    m_templateMatchingCurrentTime = cv::getTickCount();
+    double duration = (double)(m_templateMatchingCurrentTime - m_templateMatchingStartTime) / TICK_FREQUENCY;
+
+    // If template matching lasts for more than 2 seconds face is possibly lost
+    // so disable it and redetect using cascades
+    if (duration > m_templateMatchingMaxDuration) {
+        m_foundFace = false;
+        m_templateMatchingRunning = false;
+        m_templateMatchingStartTime = m_templateMatchingCurrentTime = 0;
+		m_facePosition.x = m_facePosition.y = 0;
+		m_trackedFace.x = m_trackedFace.y = m_trackedFace.width = m_trackedFace.height = 0;
+		return;
+    }
+
+	// Edge case when face exits frame while 
+	if (m_faceTemplate.rows * m_faceTemplate.cols == 0 || m_faceTemplate.rows <= 1 || m_faceTemplate.cols <= 1) {
+		m_foundFace = false;
+		m_templateMatchingRunning = false;
+		m_templateMatchingStartTime = m_templateMatchingCurrentTime = 0;
+		m_facePosition.x = m_facePosition.y = 0;
+		m_trackedFace.x = m_trackedFace.y = m_trackedFace.width = m_trackedFace.height = 0;
+		return;
+	}
+
+    // Template matching with last known face 
+    //cv::matchTemplate(frame(m_faceRoi), m_faceTemplate, m_matchingResult, CV_TM_CCOEFF);
+    cv::matchTemplate(frame(m_faceRoi), m_faceTemplate, m_matchingResult, cv::TM_SQDIFF_NORMED);
+    cv::normalize(m_matchingResult, m_matchingResult, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+    double min, max;
+    cv::Point minLoc, maxLoc;
+    cv::minMaxLoc(m_matchingResult, &min, &max, &minLoc, &maxLoc);
+    int num = 35;
+//    cv::Size s = m_matchingResult_combined.size();
+    std::string standardString = std::to_string(num);
+    godot::String godotString = godot::String(standardString.c_str());
+    godot::Godot::print(godotString);
+
+    // Add roi offset to face position
+    minLoc.x += m_faceRoi.x;
+    minLoc.y += m_faceRoi.y;
+
+    // Get detected face
+    //m_trackedFace = cv::Rect(maxLoc.x, maxLoc.y, m_trackedFace.width, m_trackedFace.height);
+    m_trackedFace = cv::Rect(minLoc.x, minLoc.y, m_faceTemplate.cols, m_faceTemplate.rows);
+    m_trackedFace = doubleRectSize(m_trackedFace, cv::Rect(0, 0, frame.cols, frame.rows));
+
+    // Get new face template
+    m_faceTemplate = getFaceTemplate(frame, m_trackedFace);
+
+    // Calculate face roi
+    m_faceRoi = doubleRectSize(m_trackedFace, cv::Rect(0, 0, frame.cols, frame.rows));
+
+    // Update face position
+    m_facePosition = centerOfRect(m_trackedFace);
+
+    // ------------------------------------------------------ modified for last known and debugging
+
+    // Template matching with last known face 
+    cv::Rect m_faceRoi_lastKnown = doubleRectSize(m_trackedFace_lastKnown, cv::Rect(0, 0, frame.cols, frame.rows));
+    cv::Mat m_matchingResult_lastKnown;
+    cv::matchTemplate(frame(m_faceRoi_lastKnown), m_faceTemplate_lastKnown_queue.front(), m_matchingResult_lastKnown, cv::TM_SQDIFF_NORMED);
+
+    cv::normalize(m_matchingResult_lastKnown, m_matchingResult_lastKnown, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+    
+    double min_last, max_last;
+    cv::Point minLoc_last, maxLoc_last;
+    
+    cv::minMaxLoc(m_matchingResult_lastKnown, &min_last, &max_last, &minLoc_last, &maxLoc_last);
+
+    minLoc_last.x += m_faceRoi_lastKnown.x;
+    minLoc_last.y += m_faceRoi_lastKnown.y;
+
+    // Display the 'last known from template' red dot
+    cv::Rect  m_trackedFace_debug = cv::Rect(minLoc_last.x, minLoc_last.y, m_faceTemplate.cols, m_faceTemplate.rows);
+    std::string standardString_lastKnown= std::to_string(minLoc_last.x) + ":" + std::to_string(minLoc_last.y);
+    m_trackedFace_debug = doubleRectSize(m_trackedFace_debug, cv::Rect(0, 0, frame.cols, frame.rows));
+
+    // Get new face template
+    cv::Mat m_faceTemplate_debug = getFaceTemplate(frame, m_trackedFace_debug);
+
+    // Calculate face roi
+    cv::Rect m_faceRoi_debug = doubleRectSize(m_trackedFace_debug, cv::Rect(0, 0, frame.cols, frame.rows));
+
+    // Update face position
+    cv::Point m_facePosition_debug = centerOfRect(m_trackedFace_debug);
+
+    cv::Point facePos_debug;
+    facePos_debug.x = (int)(m_facePosition_debug.x / m_scale);
+    facePos_debug.y = (int)(m_facePosition_debug.y / m_scale);
+    rectangle(orgFrame, cv::Rect(facePos_debug.x,facePos_debug.y,5,5), cv::Scalar(0,0,255), 4,8,0);
+}
+
+cv::Point VideoHandDetector::getFrameAndDetect(cv::Mat &frame)
+{
+    *m_videoCapture >> frame;
+
+    // Downscale frame to m_resizedWidth width - keep aspect ratio
+    m_scale = (double) std::min(m_resizedWidth, frame.cols) / frame.cols;
+    cv::Size resizedFrameSize = cv::Size((int)(m_scale*frame.cols), (int)(m_scale*frame.rows));
+
+    cv::Mat resizedFrame;
+    cv::resize(frame, resizedFrame, resizedFrameSize);
+
+    if (!m_foundFace)
+        detectFaceAllSizes(resizedFrame); // Detect using cascades over whole image
+    else {
+        detectFaceAroundRoi(resizedFrame); // Detect using cascades only in ROI
+        if (m_templateMatchingRunning) {
+            rectangle(frame, cv::Rect(0,0,100,100), cv::Scalar(255,0,0), 4,8,0);
+            detectFacesTemplateMatching(resizedFrame, frame); // Detect using template matching
+        }
+    }
+
+    return m_facePosition;
+}
+
+cv::Point VideoHandDetector::operator>>(cv::Mat &frame)
+{
+    return this->getFrameAndDetect(frame);
+}
---
+++ src/VideoHandDetector.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <queue>
+
+class VideoHandDetector
+{
+public:
+    // VideoHandDetector(const std::string cascadeFilePath, cv::VideoCapture &videoCapture);
+    VideoHandDetector();
+    ~VideoHandDetector();
+
+    cv::Point               getFrameAndDetect(cv::Mat &frame);
+    cv::Point               operator>>(cv::Mat &frame);
+    void                    setVideoCapture(cv::VideoCapture &videoCapture);
+    cv::VideoCapture*       videoCapture() const;
+    void                    setFaceCascade(const std::string cascadeFilePath);
+    cv::CascadeClassifier*  faceCascade() const;
+    void                    setResizedWidth(const int width);
+    int                     resizedWidth() const;
+	bool					isFaceFound() const;
+    cv::Rect                face() const;
+    cv::Point               facePosition() const;
+    void                    setTemplateMatchingMaxDuration(const double s);
+    double                  templateMatchingMaxDuration() const;
+    std::queue<cv::Mat>     getLastKnownFaceTemplateQueue() const;
+
+private:
+    static const double     TICK_FREQUENCY;
+
+    cv::VideoCapture*       m_videoCapture = NULL;
+    cv::CascadeClassifier*  m_faceCascade = NULL;
+    std::vector<cv::Rect>   m_allFaces;
+    cv::Rect                m_trackedFace;
+    cv::Rect                m_trackedFace_lastKnown;
+    cv::Rect                m_faceRoi;
+    cv::Mat                 m_faceTemplate;
+    std::queue<cv::Mat>     m_faceTemplate_lastKnown_queue;
+    cv::Mat                 m_matchingResult;
+    bool                    m_templateMatchingRunning = false;
+    int64                   m_templateMatchingStartTime = 0;
+    int64                   m_templateMatchingCurrentTime = 0;
+    bool                    m_foundFace = false;
+    double                  m_scale;
+    int                     m_resizedWidth = 320;
+    cv::Point               m_facePosition;
+    double                  m_templateMatchingMaxDuration = 3;
+    int                     m_maxBufferFaceTemplate = 5;
+
+    cv::Rect    doubleRectSize(const cv::Rect &inputRect, const cv::Rect &frameSize) const;
+    cv::Rect    biggestFace(std::vector<cv::Rect> &faces) const;
+    cv::Point   centerOfRect(const cv::Rect &rect) const;
+    cv::Mat     getFaceTemplate(const cv::Mat &frame, cv::Rect face);
+    void        detectFaceAllSizes(const cv::Mat &frame);
+    void        detectFaceAroundRoi(const cv::Mat &frame);
+    void        detectFacesTemplateMatching(const cv::Mat &frame, const cv::Mat &orgFrame);
+};
