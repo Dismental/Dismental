3cd612679461275bb6090f7f208dd4d9564a4ff3
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-05-22 13:51:40 +0200

    Added a util function to change scenes

--- Godot/Script/Utils.gd
+++ Godot/Script/Utils.gd
@@ -1,3 +1,4 @@
+extends Node
 
 static func _count_files_in_dir(path):
 	var count = 0
@@ -14,3 +15,15 @@ static func _count_files_in_dir(path):
 
 	dir.list_dir_end()
 	return count
+
+# Changes the node from s_node with one made from the scene resource given in path
+static func _change_screen(path: String, parent: Node, s_node : Node):
+	var node = load(path)
+	node = node.instance()
+	parent.add_child(node)
+	parent.remove_child(s_node)
+	s_node.queue_free()
+	if s_node.is_queued_for_deletion():
+		print("deleting")
+	else:
+		push_error("can't delete" + str(s_node))
