cd7342d5bd5e691173bb8d9fea312099f1231500
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-05-22 14:44:48 +0200

    Changed _change_screen to allow no parent for the nodes changed

--- Godot/Script/Utils.gd
+++ Godot/Script/Utils.gd
@@ -17,11 +17,15 @@ static func _count_files_in_dir(path):
 	return count
 
 # Changes the node from s_node with one made from the scene resource given in path
-static func _change_screen(path: String, parent: Node, s_node : Node):
+static func _change_screen(path: String, s_node : Node):
 	var node = load(path)
+	var parent = s_node.get_parent()
 	node = node.instance()
-	parent.add_child(node)
-	parent.remove_child(s_node)
+	if is_instance_valid(parent):
+		parent.add_child(node)
+		parent.remove_child(s_node)
+	else: 
+		print("node has no parent, so is connected to root!")
 	s_node.queue_free()
 	if s_node.is_queued_for_deletion():
 		print("deleting")
