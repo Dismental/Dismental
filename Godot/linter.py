d08ba6762fd698ed91ef0b92aebe4f8a296c21fe
Kevin van der Werff <k.s.a.vanderwerff@student.tudelft.nl>
2020-05-23 14:09:01 +0200

    add option to lint directory non-recursive

--- Godot/linter.py
+++ Godot/linter.py
@@ -1,18 +1,23 @@
 import os, sys
 from pathlib import Path
 
-# No arguments given
-if len(sys.argv) == 1:
-  for path in Path('./Script/').rglob('*.gd'):
-    print("Linting: " + str(path))
-    os.system(f'gdlint {path}')
-    print("---")
-  exit()
-
-# Either a file or directory is given
-path = sys.argv[1]
-if path.endswith(".gd"):
+def gdlint(path):
   print("Linting: " + str(path))
   os.system(f'gdlint {path}')
-elif path.endswith("/"):
-  pass
+  print("---")
+
+# Recursive lint all .gd files
+if len(sys.argv) == 1:
+  for path in Path('./Script/').rglob('*.gd'):
+    gdlint(path)
+  exit()
+
+path = sys.argv[1]
+if os.path.isfile(path) and path.endswith(".gd"):
+    gdlint(path)
+elif os.path.isdir(path):
+  # Non recursive
+  for file in Path(path).glob('*.gd'):
+    gdlint(file)
+else:
+  print("I could not find what you're looking for :(")
