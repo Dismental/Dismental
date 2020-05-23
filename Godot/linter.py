823475c21e45fe5ea3f15f3031bcf750509bfa0f
Kevin van der Werff <k.s.a.vanderwerff@student.tudelft.nl>
2020-05-23 13:58:07 +0200

    add option to lint specific file

--- Godot/linter.py
+++ Godot/linter.py
@@ -1,7 +1,18 @@
-import os
+import os, sys
 from pathlib import Path
 
-for path in Path('./Script/').rglob('*.gd'):
+# No arguments given
+if len(sys.argv) == 1:
+  for path in Path('./Script/').rglob('*.gd'):
+    print("Linting: " + str(path))
+    os.system(f'gdlint {path}')
+    print("---")
+  exit()
+
+# Either a file or directory is given
+path = sys.argv[1]
+if path.endswith(".gd"):
   print("Linting: " + str(path))
   os.system(f'gdlint {path}')
-  print("---")
+elif path.endswith("/"):
+  pass
