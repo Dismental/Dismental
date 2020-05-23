00adfe6ee7b5c8099bb6476ec92c362c0f72b6cb
Kevin van der Werff <k.s.a.vanderwerff@student.tudelft.nl>
2020-05-23 19:27:53 +0200

    trim newlines at end of lines .gdlintignore, additional minor improvements

--- Godot/linter.py
+++ Godot/linter.py
@@ -9,16 +9,17 @@ ignore = open("./.gdlintignore", "r")
 ignore_files = []
 for line in ignore:
   if not line.startswith("#"):
-    ignore_files.append(line)
+    ignore_files.append(line.rstrip())
 
 def gdlint(path):
-  print("Linting: " + str(path))
+  print(f"Linting: {path}")
   os.system(f'gdlint {path}')
   print("---")
 
 def validFile(path):
   return os.path.isfile(path) and path.endswith(".gd")
 
+# If no additional arguments are given
 # Recursive lint all .gd files in ./Script/
 # and non-recursively lint all .gd files in ./
 if len(sys.argv) == 1:
@@ -35,19 +36,16 @@ if len(sys.argv) == 1:
       gdlint(path)
   exit()
 
+# If it's a list of paths
 paths = sys.argv[1:]
-if len(paths) == 1:
-  path = paths[0]
-  if validFile(path):
-      gdlint(path)
-  elif os.path.isdir(path):
-    # Non recursive
+files_to_lint = []
+for path in paths:
+  if os.path.isdir(path):
     for file in Path(path).glob('*.gd'):
-      if file.name not in ignore_files:
-        gdlint(file)
+      files_to_lint.append(file)    
 
-# If more paths are given to specific files
-else:
-  for path in paths:
-    if validFile(path):
-      gdlint(path)
+  if validFile(path):
+    files_to_lint.append(path)
+
+for file in files_to_lint:
+  gdlint(file)
