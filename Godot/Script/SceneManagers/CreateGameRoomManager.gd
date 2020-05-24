502ac8f334945a7cea8047e4e8d17873f3952ac3
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-05-24 20:36:05 +0200

    Fixed method call typo

--- Godot/Script/SceneManagers/CreateGameRoomManager.gd
+++ Godot/Script/SceneManagers/CreateGameRoomManager.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_CreateRoomButton_pressed():
-	Network._create_server()
+	Network.create_server()
 	return Utils._change_screen("res://Scenes/GameRoomHost.tscn", self)
 
 func _on_BackButton_pressed():
