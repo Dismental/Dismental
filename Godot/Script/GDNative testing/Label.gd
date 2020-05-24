35511cfeddf9ef6161efde93079654c81e082b49
Kevin van der Werff <k.s.a.vanderwerff@student.tudelft.nl>
2020-05-24 20:25:39 +0200

    clean up, remove unneeded items

--- Godot/Script/GDNative testing/Label.gd
+++ Godot/Script/GDNative testing/Label.gd
@@ -5,8 +5,4 @@ func _ready():
 	get_parent().get_node("Button").connect("pressed", self, "on_Button_pressed")
 
 func on_Button_pressed():
-	print("SIODJAIUSDHp ndpo")
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	print("The button has been pressed")
