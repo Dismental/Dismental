cef03be54ff1b255435cd3a0e389b167c2d58cc8
Onno <o.m.gieling@student.tudelft.nl>
2020-05-25 09:43:54 +0200

    Revert "Merge remote-tracking branch 'origin/9-hand-tracking' into 64-improve-face-tracking"
    This reverts commit bfe1cb6626dcb83b2ff389745d35bf03b47bc4f7.

--- src/cursor.h
+++ src/cursor.h
@@ -6,9 +6,7 @@
 #include <opencv2/core/mat.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/objdetect.hpp>
-#include <opencv2/tracking.hpp>
 #include "VideoFaceDetector.h"
-#include "VideoHandDetector.h"
 
 namespace godot {
 
@@ -20,20 +18,13 @@ private:
     float time_emit;
     float amplitude;
     float speed;
-    bool waitingForSample;
     cv::Mat image;
     cv::Mat frame;
     cv::VideoCapture camera;
     cv::CascadeClassifier face_cascase;
     cv::Point cursorPos;
 
-    // Hand tracking
-    cv::Ptr<cv::Tracker> tracker;
-    cv::Rect2d bbox;
-    cv::Mat handSample;
-
     VideoFaceDetector detector;
-    VideoHandDetector handTracker;
 
 public:
     static void _register_methods();
