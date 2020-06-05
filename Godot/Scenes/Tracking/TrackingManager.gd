cfe3a0ade1bbc26709f365c08df54188e367f807
Gees Brouwer <G.D.Brouwer@student.tudelft.nl>
2020-06-05 03:24:46 +0200

    Added comments

--- Godot/Scenes/Tracking/TrackingManager.gd
+++ Godot/Scenes/Tracking/TrackingManager.gd
@@ -36,8 +36,7 @@ var free_movement_zone_warp = 1
 var throttle_zone_warp = 1
 
 var delta_angle = 0
-var delta_delta_angle = 0
-var delta_distance = 0
+var delta_angle_diff = 0
 var movement_speed = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
@@ -50,16 +49,30 @@ func _process(_delta):
 	if (role == ROLE.debug):
 		
 		var tracking_pos_new = _map_tracking_position(tracking_node.position)
+		
+		# The distance between the last tracking position and the new tracking position
 		var distance_new = (tracking_pos_new.distance_to(tracking_pos))
 		
-		delta_delta_angle = atan2(tracking_pos_new.y - tracking_pos.y, tracking_pos_new.x - tracking_pos.x) - delta_angle
+		var delta_angle_old = delta_angle
+		# The angle between the last tracking position and the new tracking position e.g. the direction the tracking_pos moved
 		delta_angle = atan2(tracking_pos_new.y - tracking_pos.y, tracking_pos_new.x - tracking_pos.x)
-		delta_distance = tracking_pos.distance_to(tracking_pos_new)
+		# The difference between last angle and new angle e.g. how much the direction changes
+		delta_angle_diff = delta_angle - delta_angle_old
 		
-		
+		# The current position of the pointer
 		pointer_pos_current = pointer_node.position
+		
+		# The speed by which the pointer moves in pixels per second
 		movement_speed = (pointer_pos_current - pointer_pos) / _delta
 		
+		# There are two zones: the free movement zone and the throttle zone
+		# One can see the throttle zone as a 'warning zone' in which the speed of the pointer is throttled
+		# When outside of the throttle zone, the tracking will be lost
+		# By applying this, the pointer is insensitive for sudden movements by errors or faces of a bystander
+		# The zones should allow more movement in the direction the pointer is already moving and should be sensitive for sudden 'jumps'
+		# In the ideal case the zones should resemble ellipses that are warped according to the movement speed
+		
+		# Defining the warping of the free movement zone and the throttle zone
 		if (lost_tracking): 
 			free_movement_zone_warp = 1
 			throttle_zone_warp = 1
@@ -75,7 +88,7 @@ func _process(_delta):
 		if (distance_new < 1):
 			distance_new = 1
 		
-
+		# Check if the new tracking position is inside of the free movement zone
 		if _within_free_movement_zone(tracking_pos, tracking_pos_new):
 			if (lost_tracking): 
 				lost_tracking = false
@@ -84,7 +97,8 @@ func _process(_delta):
 				
 			var position_offset = tracking_pos_new - tracking_pos
 			
-			var factor = 2.5 + abs(sin(delta_delta_angle)) * 6
+			# Define the movement of the pointer in the direction to tracking_pos according to the change of direction
+			var factor = 2.5 + abs(sin(delta_angle_diff)) * 6
 			if (movement_speed.length() < 200):
 				tracking_pos += (position_offset/4)
 			else:
@@ -93,6 +107,7 @@ func _process(_delta):
 			
 				
 			pointer_node.position = tracking_pos
+		# check if the new tracking position is inside of the throttle zone
 		elif _within_throttled_zone(tracking_pos, tracking_pos_new):
 #			print("Throttled")
 			var distance_outside_free_zone = distance_new - free_movement_zone_radius
@@ -107,7 +122,7 @@ func _process(_delta):
 				position_offset_limited_to_edge = position_offset_norm * _distance_to_free_zone_edge(tracking_pos, tracking_pos_new)
 				tracking_pos += position_offset_limited_to_edge/4
 				pointer_node.position = tracking_pos
-			
+		# tracking is lost
 		else:
 			if not (lost_tracking): 
 				print("Lost")
@@ -129,9 +144,11 @@ func _process(_delta):
 func _within_free_movement_zone(pos, pos_new):
 	var distance = (pos.distance_to(pos_new))
 	
+	# calculate the relevant point on the ellipse-shaped free movement zone
 	var ellipse_point = point_on_ellipse(0, delta_angle, free_movement_zone_radius, free_movement_zone_warp)
 	
 	#print(ellipse_point.length())
+	# check if the distance of pos is smaller than the radius of the point on the ellipse
 	if (distance < ellipse_point.length()):
 		return true
 	return false
@@ -140,15 +157,18 @@ func _within_free_movement_zone(pos, pos_new):
 func _within_throttled_zone(pos, pos_new):
 	var distance = (pos.distance_to(pos_new))
 	
+	# calculate the relevant point on the ellipse-shaped free movement zone
 	var ellipse_point = point_on_ellipse(0, delta_angle, throttle_zone_radius, throttle_zone_warp)
 	
 	#print(ellipse_point.length())
+	# check if the distance of pos is smaller than the radius of the point on the ellipse
 	if (distance < ellipse_point.length()):
 #		print("in throttle zone")
 		return true
 	return false
 
 func _distance_to_free_zone_edge(pos, pos_new):
+	# calculate the relevant point on the ellipse-shaped free movement zone
 	var ellipse_point = point_on_ellipse(delta_angle, delta_angle, free_movement_zone_radius, free_movement_zone_warp)
 	return ellipse_point.length()
 
@@ -198,6 +218,8 @@ func _on_Sprite_draw():
 	print ("draw sprite")
 	
 
+# calculate the x and y of a point defined by 'angle' on an ellipse with center=0,0, a specific rotation, a radius and a level of warping
+# this function is used for defining the boundries of an ellipse-shaped zone 
 func point_on_ellipse(angle, rotation, radius, warp):
 	return Vector2(
 		cos(rotation) * cos(angle) * (radius * warp) - sin(rotation) * sin(angle) * (radius) + cos(rotation) * radius * (warp - 1) / 1.61803398875,
