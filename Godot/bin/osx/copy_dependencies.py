e9194534271c54ddc3897d291b8776410344164a
Kevin van der Werff <k.s.a.vanderwerff@student.tudelft.nl>
2020-06-09 14:22:57 +0200

    remove appending to added_libs.txt, cause you can just run a command to retrieve the list of dylibs from osx/ to add to .gdnlib

--- Godot/bin/osx/copy_dependencies.py
+++ Godot/bin/osx/copy_dependencies.py
@@ -2,14 +2,13 @@ import glob, subprocess, sys, os, argparse
 from os import path
 
 """
-This script will do the following three things
+This script will execute the following steps:
 1.  For all .dylib files (excluding symlinks), look for any LC_LOAD_DYLIB's that start with the given PREFIX.
 2.  For those LC_LOAD_DYLIB's that start with the PREFIX, look whether the file that it points to already exists in the current directory that the script is running from.
     For example, if you have a PREFIX=/usr/local/opt and if you have a LC_LOAD_DYLIB="/usr/local/opt/ffmpeg/processimg.dylib" on a certain .dylib file,
     this script will check if processimg.dylib exists in the current directory.
     2.1.  If it does exist, it won't copy the dylib, skip to step 3.
     2.2.  If it does not exist, it will copy the .dylib over from the location mentioned in LC_LOAD_DYLIB to the current directory.
-          It will also append a line "res://bin/osx/lib/processimg.dylib" to added_libs.txt, the lines added there are meant to be copied to .gdnlib. Once that's done, the .txt file should cleared.
 3.  After this, it will change the LC_LOAD_DYLIB path to @loader_path/processimg.dylib.
 
 Run `python copy_dependencies.py -h` for information on the arguments you can pass.
@@ -41,7 +40,6 @@ NEW_LOCATION = "@loader_path/"
 # Look up all the files that match the glob, symlinks excluded
 dylibs = command_to_list("ls -ld " + args.glob + "| grep -v ^l | awk '{print $9}'")
 
-added = []
 omitted = []
 for lib in dylibs:
   # Remove first 2 lines, first line is filename, second is reference to itself
@@ -72,18 +70,9 @@ for lib in dylibs:
 
     if (args.e):
       if not lib_exists:
-        # Append path to .txt that needs to be copied to .gdnlib
-        added.append(f"res://bin/osx/{target_lib_name}")
         subprocess.run(copy_cmd, shell=True)
       subprocess.run(change_LC_LOAD_DYLIB_cmd, shell=True)
 
 print()
-print("Scanned the following files as well, but did not find the given prefix.")
+print("Scanned the following files as well, but did not find the given prefix in any of the paths refered to by the file.")
 print(omitted)
-
-# Write added files to .txt
-# so that we can add them to dylib afterwards
-f = open("added_libs.txt", "a")
-for add in added:
-  f.write(f'{add}\n')
-f.close()
