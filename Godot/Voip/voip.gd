98075bb218985b89edb5f87a556113fd3d6db457
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-06-13 17:05:49 +0200

    compressed and shortened audio-packets to fix invalid data error

--- Godot/Voip/voip.gd
+++ Godot/Voip/voip.gd
@@ -1,6 +1,6 @@
 extends AudioStreamPlayer
 
-const MIN_PACKET_LENGTH = 1
+const MIN_PACKET_LENGTH = 0.4
 
 var mic : AudioEffectRecord
 var record
@@ -10,11 +10,11 @@ var time_elapsed = 0
 func _ready():
 	mic = AudioServer.get_bus_effect(AudioServer.get_bus_index("Record"), 0)
 
-remote func _play(id, audioPacket, format, mix_rate, stereo):
+remote func _play(id, audioPacket : PoolByteArray, format, mix_rate, stereo, size):
 	print("received audio from player with id: %s\n" % id)
-
+	var dec = audioPacket.decompress(size, 1)
 	var audioStream = AudioStreamSample.new()
-	audioStream.data = audioPacket
+	audioStream.data = dec
 	audioStream.set_format(format)
 	audioStream.set_mix_rate(mix_rate)
 	audioStream.set_stereo(stereo)
@@ -27,8 +27,11 @@ func _process(delta: float) -> void:
 			if time_elapsed >= MIN_PACKET_LENGTH:
 				mic.set_recording_active(false)
 				record = mic.get_recording()
-				rpc_unreliable("_play",get_tree().get_network_unique_id(), record.get_data(), record.get_format(), record.get_mix_rate(), record.is_stereo())
-				print("send recording of size %s\n" % record.get_data().size())
+				var comp = record.get_data().compress(1)
+				rpc_unreliable("_play",get_tree().get_network_unique_id(), comp, record.get_format(), record.get_mix_rate(), record.is_stereo(), record.get_data().size())
+				if(comp.size() > 50000):
+					print("send recording of size %s\n" % comp.size())
+#				print("original size: " + str(record.get_data().size()))
 				mic.set_recording_active(true)
 				time_elapsed = 0
 				
