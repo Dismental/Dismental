948152d32159b797ef1d3063bd35e73f408755b0
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-06-14 17:41:03 +0200

    Workaround for fatal p_index out of bounds error in CoWdata.h

--- Godot/Voip/voip.gd
+++ Godot/Voip/voip.gd
@@ -9,6 +9,7 @@ var time_elapsed = 0
 
 func _ready():
 	mic = AudioServer.get_bus_effect(AudioServer.get_bus_index("Record"), 1)
+	print("voip added")
 
 remote func _play(id, audioPacket : PoolByteArray, format, mix_rate, stereo, size):
 	#print("received audio from player with id: %s\n" % id)
@@ -23,24 +24,24 @@ remote func _play(id, audioPacket : PoolByteArray, format, mix_rate, stereo, siz
 		audioStream.set_stereo(stereo)
 		stream = audioStream
 		play()
-	
+
+func _helper():
+	record = mic.get_recording()
+	mic.set_recording_active(true)
+	if (record == null):
+		print("recording is null!")
+	else:
+		var comp = record.get_data().compress(1)
+		rpc("_play",get_tree().get_network_unique_id(), comp, record.get_format(), 
+		record.get_mix_rate(), record.is_stereo(), record.get_data().size())
+	time_elapsed = 0
+
 func _process(delta: float) -> void:
 	if recording:
 		if mic.is_recording_active():
 			if time_elapsed >= MIN_PACKET_LENGTH:
 				mic.set_recording_active(false)
-				record = mic.get_recording()
-				if (record == null):
-					print("recording is null!")
-				else:
-					var comp = record.get_data().compress(1)
-					rpc("_play",get_tree().get_network_unique_id(), comp, record.get_format(), record.get_mix_rate(), record.is_stereo(), record.get_data().size())
-	#				if(comp.size() > 20000):
-	#					print("send recording of size %s\n" % comp.size())
-	#				print("original size: " + str(record.get_data().size()))
-					mic.set_recording_active(true)
-					time_elapsed = 0
-				
+				call_deferred("_helper")
 			time_elapsed += delta
 		else:
 			mic.set_recording_active(true)
