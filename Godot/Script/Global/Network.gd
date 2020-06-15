f1b8ca0feffe51fecda90b7bf1770900a83a01ce
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-06-15 18:30:36 +0200

    Switch some spaces to tabs

--- Godot/Script/Global/Network.gd
+++ Godot/Script/Global/Network.gd
@@ -71,12 +71,12 @@ func disconnected():
 		print("host disconnect")
 		stop()
 		Utils.change_screen("res://Scenes/JoinGameRoom.tscn",
-		 get_tree().get_current_scene().get_child(0))
+			get_tree().get_current_scene().get_child(0))
 		get_tree().get_current_scene().get_node("JoinGameRoom").popup(
 			"Host disconnected")
 	if code == 4004:
 		Utils.change_screen("res://Scenes/JoinGameRoom.tscn",
-		 get_tree().get_current_scene().get_node("GameRoomPlayer"))
+			get_tree().get_current_scene().get_node("GameRoomPlayer"))
 		get_tree().get_current_scene().get_node("JoinGameRoom").popup(
 			"Room with that name does not exist")
 
