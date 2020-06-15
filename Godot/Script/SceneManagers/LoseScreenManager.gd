d3299b05c4ebc63909a8be0683fb1e66900fca26
Pepijn <p.e.f.klop@student.tudelft.nl>
2020-06-15 16:13:37 +0200

    Disconnect from peer network when going to main menu

--- Godot/Script/SceneManagers/LoseScreenManager.gd
+++ Godot/Script/SceneManagers/LoseScreenManager.gd
@@ -3,4 +3,5 @@ extends Control
 
 
 func _on_MainMenuButton_pressed():
+	Network.stop()
 	return Utils.change_screen("res://Scenes/MainMenu.tscn", self)
--- Godot/Script/SceneManagers/WinScreenManager.gd
+++ Godot/Script/SceneManagers/WinScreenManager.gd
@@ -1,6 +1,6 @@
 extends Control
 
 
-
 func _on_MainMenuButton_pressed():
+	Network.stop()
 	return Utils.change_screen("res://Scenes/MainMenu.tscn", self)
