ab70a8491ccdc9ef641cf5c0e963550d9410f638
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-06-16 16:03:36 +0200

    changed if to elif

--- Godot/Script/Global/Network.gd
+++ Godot/Script/Global/Network.gd
@@ -74,7 +74,7 @@ func disconnected():
 			get_tree().get_current_scene().get_child(0))
 		get_tree().get_current_scene().get_node("JoinGameRoom").popup(
 			"Host disconnected")
-	if code == 4004:
+	elif code == 4004:
 		Utils.change_screen("res://Scenes/JoinGameRoom.tscn",
 			get_tree().get_current_scene().get_node("GameRoomPlayer"))
 		get_tree().get_current_scene().get_node("JoinGameRoom").popup(
