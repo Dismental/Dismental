0a4da1f3ff165c1383d91e41f930d264e4df0886
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-06-16 16:09:51 +0200

    Close VoiceStream when leaving win/lose screen

--- Godot/Script/SceneManagers/LoseSceneManager.gd
+++ Godot/Script/SceneManagers/LoseSceneManager.gd
@@ -2,5 +2,10 @@ extends Control
 
 
 func _on_MainMenuButton_pressed():
+	var voice = get_parent().get_node("VoiceStream")
+	if (voice != null):
+		voice.stop()
+		voice.get_parent().remove_child(voice)
+		voice.queue_free()
 	Network.stop()
 	return Utils.change_screen("res://Scenes/MainMenu.tscn", self)
