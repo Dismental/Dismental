80ec625be6c1e52410f55d7c8727d05ae233d859
Gees Brouwer <G.D.Brouwer@student.tudelft.nl>
2020-06-18 01:21:11 +0200

    Main menu mission panels and animations

--- Godot/Scenes/NewMain.gd
+++ Godot/Scenes/NewMain.gd
@@ -5,29 +5,46 @@ extends Control
 # var a = 2
 # var b = "text"
 
-var scroll_down_animation = false
+var scroll_down_animating = false
 var scroll_down_anim_progress = 0
-const scroll_down_amount = 500
-const scroll_down_anim_duration = 1 / 60 / .7
+var scroll_down_amount = Vector2(0,0)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var createMissionPanel = $CreateMissionPanel
+	print(createMissionPanel.get_rect())
 	pass # Replace with function body.
 	
 func calc_scroll_pos(progress):
-	return (1.0 - pow(1.0 - progress, 6)) * scroll_down_amount
+	var frac = (1.0 - pow(1.0 - progress, 6))
+	return scroll_down_amount * frac
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if scroll_down_animation:
+	if scroll_down_animating:
 		scroll_down_anim_progress += 1 * delta
 		scroll_down_anim_progress = min(scroll_down_anim_progress, 1)
-		self.margin_top = -calc_scroll_pos(scroll_down_anim_progress)
+		self.margin_top = -calc_scroll_pos(scroll_down_anim_progress).y
+		self.margin_left = -calc_scroll_pos(scroll_down_anim_progress).x
+		$SettingsPanel.margin_top = 515 - self.margin_top
 		if scroll_down_anim_progress >= 1:
-			scroll_down_animation = false
+			scroll_down_animating = false
 
 
 func _on_Button2_pressed():
-	scroll_down_animation = true
+	var centerPanel = $CreateMissionPanel.get_rect().position + $CreateMissionPanel.get_rect().size / 2
+	var centerScreen = self.get_rect().position + self.get_rect().size / 2
+	scroll_down_animating = true
+	scroll_down_amount.x = centerPanel.x - centerScreen.x
+	scroll_down_amount.y = centerPanel.y - centerScreen.y
+
+
+func _on_JoinMissionButton_pressed():
+	var centerPanel = $JoinMissionPanel.get_rect().position + $JoinMissionPanel.get_rect().size / 2
+	var centerScreen = self.get_rect().position + self.get_rect().size / 2
+	scroll_down_animating = true
+	print(centerPanel.y)
+	scroll_down_amount.x = centerPanel.x - centerScreen.x
+	scroll_down_amount.y = centerPanel.y - centerScreen.y
