318a82992aa08a1df91af3e357873175d046c03b
Gees Brouwer <G.D.Brouwer@student.tudelft.nl>
2020-06-20 12:59:56 +0200

    Main menu Join/create mission panel

--- Godot/Scenes/NewMain.gd
+++ Godot/Scenes/NewMain.gd
@@ -5,47 +5,39 @@ extends Control
 # var a = 2
 # var b = "text"
 
-var scroll_down_animating = false
-var scroll_down_anim_progress = 0
-var scroll_down_amount = Vector2(0,0)
+var scrollDownAnimating = false
+var scrollDownProgress = 0
+var scrollDownAmount = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var createMissionPanel = $CreateMissionPanel
-	print(createMissionPanel.get_rect())
+	scrollDownAmount = $Background.get_rect().size.y - self.get_rect().size.y
 	pass # Replace with function body.
 	
 func calc_scroll_pos(progress):
 	var frac = (1.0 - pow(1.0 - progress, 6))
-	return scroll_down_amount * frac
+	return scrollDownAmount * frac
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if scroll_down_animating:
-		scroll_down_anim_progress += 1 * delta
-		scroll_down_anim_progress = min(scroll_down_anim_progress, 1)
-		self.margin_top = -calc_scroll_pos(scroll_down_anim_progress).y
-		self.margin_left = -calc_scroll_pos(scroll_down_anim_progress).x
+	if scrollDownAnimating and scrollDownProgress <= 1:
+		scrollDownProgress += 1 * delta
+		self.margin_top = -calc_scroll_pos(scrollDownProgress)
 		$SettingsPanel.margin_top = 515 - self.margin_top / 2
 		$SettingsPanel.margin_bottom = 515 - self.margin_top / 2 + $SettingsPanel.get_rect().size.y
-		if scroll_down_anim_progress >= 1:
-			scroll_down_animating = false
+		if scrollDownProgress >= 1:
+			scrollDownAnimating = false
 
 
 func _on_Button2_pressed():
-	var centerPanel = $CreateMissionPanel.get_rect().position + $CreateMissionPanel.get_rect().size / 2
-	var centerScreen = self.get_rect().position + self.get_rect().size / 2
-	scroll_down_animating = true
-	scroll_down_amount.x = centerPanel.x - centerScreen.x
-	scroll_down_amount.y = centerPanel.y - centerScreen.y
+	$MissionPanel/CreateMissionPanel.visible = true
+	$MissionPanel/JoinMissionPanel.visible = false
+	scrollDownAnimating = true
 
 
 func _on_JoinMissionButton_pressed():
-	var centerPanel = $JoinMissionPanel.get_rect().position + $JoinMissionPanel.get_rect().size / 2
-	var centerScreen = self.get_rect().position + self.get_rect().size / 2
-	scroll_down_animating = true
-	print(centerPanel.y)
-	scroll_down_amount.x = centerPanel.x - centerScreen.x
-	scroll_down_amount.y = centerPanel.y - centerScreen.y
+	$MissionPanel/CreateMissionPanel.visible = false
+	$MissionPanel/JoinMissionPanel.visible = true
+	scrollDownAnimating = true
