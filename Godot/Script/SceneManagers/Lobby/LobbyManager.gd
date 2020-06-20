b5e22d2e0a99466102385b3caa72c497c0fc3678
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-06-20 21:59:33 +0200

    Added host check to lobbyManager

--- Godot/Script/SceneManagers/Lobby/LobbyManager.gd
+++ Godot/Script/SceneManagers/Lobby/LobbyManager.gd
@@ -10,20 +10,21 @@ onready var player_nodes = [
 ]
 
 func _ready():
+	Network.connect("lobby_joined", self, "lobby_joined")
+
+	for i in range(len(players)):
+		player_nodes[i].visible = true
+		player_nodes[i].get_node("Label").set_text(players[i])
+
+
+func lobby_joined(miss_id):
+	$MissionID.text = miss_id
 	var is_host = Network.host == get_tree().get_network_unique_id()
 	$TeamNameInput.editable = is_host
 	$DifficultyBtn.disabled = not is_host
 	$WaitingForHostLbl.visible = not is_host
 	$StartMission.visible = is_host
 	$CancelMission.visible = is_host
-	
-	Network.connect("lobby_joined", self, "lobby_joined")
-	for i in range(len(players)):
-		player_nodes[i].visible = true
-		player_nodes[i].get_node("Label").set_text(players[i])
-
-func lobby_joined(miss_id):
-	$MissionID.text = miss_id
 
 func add_player(name):
 	players.append(name)
@@ -39,4 +40,3 @@ func refresh():
 	for i in range(len(players)):
 		player_nodes[i].visible = true
 		player_nodes[i].get_node("Label").set_text(players[i])
-
