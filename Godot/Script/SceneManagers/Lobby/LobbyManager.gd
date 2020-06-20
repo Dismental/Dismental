7afed2554b9e5a15aa80ea6f0a12bde70d114d30
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-06-20 22:23:06 +0200

    Add voip to lobby

--- Godot/Script/SceneManagers/Lobby/LobbyManager.gd
+++ Godot/Script/SceneManagers/Lobby/LobbyManager.gd
@@ -7,13 +7,18 @@ onready var player_nodes = [
 	$PlayersPanel/MarginContainer/VBoxContainer/VBoxContainer/Player4
 ]
 
+var voice: Node
+
 func _ready():
 	Network.connect("lobby_joined", self, "lobby_joined")
 	Network.connect("player_list_changed", self, "refresh_lobby")
 	
 	var name = Network.player_name + " (You)"
 	$PlayersPanel/MarginContainer/VBoxContainer/VBoxContainer/You/Label.text = name
-
+	
+	Utils.add_scene("res://Scenes/VoiceStream.tscn", get_parent())
+	voice = get_parent().get_node("VoiceStream")
+	voice.start()
 
 func lobby_joined(miss_id):
 	$MissionID.text = miss_id
@@ -23,6 +28,12 @@ func lobby_joined(miss_id):
 	$WaitingForHostLbl.visible = not is_host
 	$StartMission.visible = is_host
 	$CancelMission.visible = is_host
+
+
+func stop_voip():
+	voice.stop()
+	voice.get_parent().remove_child(voice)
+	voice.queue_free()
 
 
 func refresh_lobby():
