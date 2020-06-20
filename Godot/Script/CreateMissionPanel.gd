0a36974532974059f94304195d50d5282672c345
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-06-20 21:05:07 +0200

    Add empty name check for create lobby

--- Godot/Script/CreateMissionPanel.gd
+++ Godot/Script/CreateMissionPanel.gd
@@ -1,11 +1,5 @@
 extends MarginContainer
 
-var limit = 16
-
-var current_text = ''
-var cursor_line = 0
-var cursor_column = 0
-
 func _on_CreateMissionBtn_pressed():
 	if get_input_playername().empty():
 		popup("Playername can't be empty!")
@@ -16,10 +10,9 @@ func _on_CreateMissionBtn_pressed():
 
 
 func popup(text: String):
-#	$AcceptDialog.dialog_text = text
-#	$AcceptDialog.popup_centered()
-# TODO implement custom popup dialog
-	pass
+	var p_up = get_parent().get_parent().get_node("Popup")
+	p_up.change_text(text)
+	p_up.popup_centered()
 
 
 func get_input_playername():
