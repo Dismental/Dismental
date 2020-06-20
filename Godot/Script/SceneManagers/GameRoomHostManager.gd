079cf714fc97566bcb479414758224f7c47c8a38
Onno <o.m.gieling@student.tudelft.nl>
2020-06-20 13:56:47 +0200

    Add contract scene and pointer interaction

--- Godot/Script/SceneManagers/GameRoomHostManager.gd
+++ Godot/Script/SceneManagers/GameRoomHostManager.gd
@@ -1,6 +1,14 @@
 extends Control
 var voice: Node
 
+const LOADING_ROOM = "[loading]"
+const CONTRACT_REQUIRED = true
+
+var setRoomCode = true
+var contractScene
+var contract_control
+var contract_opened = false
+
 func _init():
 	Network.connect("player_list_changed", self, "refresh_lobby")
 
@@ -10,7 +18,30 @@ func _ready():
 	Utils.add_scene("res://Scenes/VoiceStream.tscn", get_parent())
 	voice = get_parent().get_node("VoiceStream")
 	voice.start()
+	
+	if CONTRACT_REQUIRED:
+		var ContractClass = preload("res://Scenes/Contract.tscn")
+		contractScene = ContractClass.instance()
+		self.add_child(contractScene)
+		contract_control = contractScene.get_node(".")
+		contract_opened = true
 
+
+func _process(_delta):
+	if setRoomCode:
+		# add the roomcode to a lbl when it is available
+		if not Network.gamelobbycode == "":
+			get_node("lbl_roomcode_code").text = Network.gamelobbycode
+			if CONTRACT_REQUIRED:
+				contractScene.get_node("lbl_roomcode").text = "Roomcode: \n " + Network.gamelobbycode
+			setRoomCode = false
+
+	if contract_opened and CONTRACT_REQUIRED:
+		if contract_control.contract_is_signed:
+			self.remove_child(contractScene)
+			contract_opened = false
+			
+	update()
 
 func _on_BackButton_pressed():
 	stop_voip()
