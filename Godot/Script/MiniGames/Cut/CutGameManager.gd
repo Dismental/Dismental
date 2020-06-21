301b609ec262b6f27f1d4c082f82107d48d89a38
Gees Brouwer <G.D.Brouwer@student.tudelft.nl>
2020-06-21 22:22:59 +0200

    Change _game_over method in Cut

--- Godot/Script/MiniGames/Cut/CutGameManager.gd
+++ Godot/Script/MiniGames/Cut/CutGameManager.gd
@@ -266,10 +266,14 @@ func _get_map_pixel_color(pos):
 	return pixelcolor
 
 
-func _game_over():
-	if player_role == Role.DEFUSER:
-		game_over_dialog.popup()
-	rpc("_on_update_running", false)
+#func _game_over():
+#	if player_role == Role.DEFUSER:
+#		game_over_dialog.popup()
+#	rpc("_on_update_running", false)
+
+remotesync func _game_over():
+	get_tree().get_root().get_node("GameScene").game_over()
+	get_parent().call_deferred("remove_child", self)
 
 
 func _game_completed():
