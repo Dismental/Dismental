d8945d221443dc9f5f5a73481663777756e6a0d3
Jeroen Janssen <j.d.janssen@student.tudelft.nl>
2020-06-21 19:00:04 +0200

    Add tests for update_team_name function

--- Godot/test/unit/gamestate_test.gd
+++ Godot/test/unit/gamestate_test.gd
@@ -11,3 +11,11 @@ func test_timer_timout_signal():
 	watch_signals(GameState)
 	GameState._on_timer_timeout()
 	assert_signal_emitted(GameState, "timer_timeout")
+
+
+func test_update_team_name():
+	watch_signals(GameState)
+	assert_eq(GameState.team_name, "BITs")
+	GameState.update_team_name("test")
+	assert_signal_emitted(GameState, "update_team_name")
+	assert_eq(GameState.team_name, "test")
