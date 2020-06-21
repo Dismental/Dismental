2294df976bce896665e3a7bd31a69545b418e358
Onno <o.m.gieling@student.tudelft.nl>
2020-06-21 14:46:01 +0200

    gdnative Face tracking cleanup

--- src/gdexample.cpp
+++ src/gdexample.cpp
@@ -17,7 +17,7 @@ using namespace cv;
 using namespace std;
 using namespace godot;
 
-const bool debug_mode = true;
+const bool debug_mode = false;
 
 void GDExample::_register_methods() {
     register_method("_process", &GDExample::_process);
@@ -46,12 +46,6 @@ string get_env_var( std::string const & key ) {
 }
 
 void GDExample::_init() {
-    // initialize any variables here
-    cursorPos = Point(frame.cols / 4, frame.rows / 4);
-
-    waitingForSample = true;
-
-    bbox = Rect2d(100,200,200,300);
 
     // TODO get .xml from res:// instead of a hardcoded path
     std::string path = "../src/opencv_data/haarcascades/haarcascade_frontalface_default.xml";
@@ -69,7 +63,6 @@ void GDExample::_init() {
     }
 
     camera.open(0); //open camera
-    camera.read(frame);
 
     // Set the handtracking Detector with the camera
     detector.setVideoCapture(camera);
@@ -79,45 +72,36 @@ void GDExample::_init() {
 
 void GDExample::_process(float delta) {
     if (camera.isOpened()) {
-    detector >> frame;
+        detector >> frame;
 
-    // handTracker.update(frame, bbox);
+        // Set the position of the linked node (should be a Position2D node) with the tracked position
+        cursorPos = Vector2(abs((float)detector.facePosition().x/(float)frame.cols-1), (float)detector.facePosition().y/(float)frame.rows);
+        set_position(cursorPos);
 
-    // if(waitKey(10) == 32) {
-    //     handTracker.toggleTracking(frame, bbox);
-    // }
-
-    // Create the 'joystick' effect by restraining the movement of cursorPos. CursorPos 'follows' facePosition and is not mapped 1on1.
-    cursorPos.x = detector.facePosition().x;
-    cursorPos.y = detector.facePosition().y;
-
-    // Set the position of the linked node (should be a Position2D node) with the tracked position
-    set_position(Vector2(abs((float)cursorPos.x/(float)frame.cols-1), (float)cursorPos.y/(float)frame.rows));
-
-    if (debug_mode)
-    {
-        // Display debug information of the tracking in the webcam frame
-        if(detector.isFaceFound()) 
+        if (debug_mode)
         {
-            rectangle(frame, detector.face(), Scalar(255,0,0), 4,8,0);
-            circle(frame, detector.facePosition(), 30, Scalar(0, 255, 0), 4,8,0);
+            // Display debug information of the tracking in the webcam frame
+            if(detector.isFaceFound()) 
+            {
+                rectangle(frame, detector.face(), Scalar(255,0,0), 4,8,0);
+                circle(frame, detector.facePosition(), 30, Scalar(0, 255, 0), 4,8,0);
+            }
+            
+            // Flip the frame to make the webcam look like a mirror.
+            cv::Mat flipFrame;
+            flip(frame, flipFrame, 1);
+            imshow("", flipFrame); // Uncomment this line to show the webcam frames in a seperate window
         }
-        
-        // Flip the frame to make the webcam look like a mirror.
-        cv::Mat flipFrame;
-        flip(frame, flipFrame, 1);
-        imshow("", flipFrame); // Uncomment this line to show the webcam frames in a seperate window
-    }
 
-    multiple_faces = detector.multiple_faces;
-    too_close = detector.too_close;
-    lost_tracking = detector.lost_tracking;
-    template_matching = detector.template_matching;
+        multiple_faces = detector.multiple_faces;
+        too_close = detector.too_close;
+        lost_tracking = detector.lost_tracking;
+        template_matching = detector.template_matching;
     } else {
         retry_timer += delta;
         if (retry_timer > 2) {
             retry_timer = 0;
             camera.open(0);
+        }
     }
-}
 }
