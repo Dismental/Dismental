a0cf986938f3b5f75179a0fb23498a502393fa48
Gees Brouwer <G.D.Brouwer@student.tudelft.nl>
2020-06-21 17:04:01 +0200

    Resolve magic numbers

--- Godot/Script/MiniGames/Cut/TextureRect.gd
+++ Godot/Script/MiniGames/Cut/TextureRect.gd
@@ -1,10 +1,18 @@
 extends TextureRect
 
+const PATTERN_SIZE = 96
 
-var pos = Vector2(0,0)
-
+var pattern_movement = 0
 
 func _process(_delta):
-	pos.x = int(pos.x + 1) % 96 - 96
-	self.margin_left = pos.x
-	self.modulate = Color(1,1,1, 1.0/3 + abs(sin(PI*(pos.x / 96))) / 3)
+	# Move pattern from -96 to 0
+	# By looping this, the red lines in the pattern appear to be constantly moving to the right
+	pattern_movement = int(pattern_movement + 1) % PATTERN_SIZE - PATTERN_SIZE
+	self.margin_left = pattern_movement
+	
+	# Use the movement value of the pattern to let the pattern blink slowly
+	# by setting the alpha value to a sin function of the movement.
+	# This leads in the pattern slowly blinking between alpha=1/3 and alpha=2/3
+	var alpha_base_val = 1.0/3
+	var alpha_variation = abs(sin(PI*(float(pattern_movement) / 96))) / 3
+	self.modulate = Color(1,1,1, alpha_base_val + alpha_variation)
