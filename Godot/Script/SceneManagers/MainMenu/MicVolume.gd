66e874a531ef581a10d6ef12dbb91785549fbe36
Jonas Duifs <j.s.duifs@student.tudelft.nl>
2020-06-21 21:47:55 +0200

    Change not working mic volume slider to voice chat  volume slider

--- Godot/Script/SceneManagers/MainMenu/MicVolume.gd
+++ Godot/Script/SceneManagers/MainMenu/MicVolume.gd
@@ -3,4 +3,4 @@ extends Label
 
 func _on_HSlider_value_changed(value):
 	self.set_text(str(value * 100, "%"))
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Record"), linear2db(value))
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("VoiceChat"), linear2db(value))
