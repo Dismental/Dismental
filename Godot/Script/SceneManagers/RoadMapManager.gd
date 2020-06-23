37759e63649aaf89a31c438be3504df7f5f84fea
Kevin van der Werff <k.s.a.vanderwerff@student.tudelft.nl>
2020-06-23 14:59:25 +0200

    1 sec -> 3 sec

--- Godot/Script/SceneManagers/RoadMapManager.gd
+++ Godot/Script/SceneManagers/RoadMapManager.gd
@@ -155,5 +155,5 @@ func _process(_delta):
 	update_instruction_animation(_delta)
 
 	# Enable next minigame button when the animations are finished
-	if $StartNextTask.disabled and opening_progress >= 1 and instruction_show_progress >= 1:
+	if $StartNextTask.disabled and opening_progress >= 3 and instruction_show_progress >= 3:
 		$StartNextTask.disabled = false
